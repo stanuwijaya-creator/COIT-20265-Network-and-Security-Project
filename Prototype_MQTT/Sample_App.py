116093e2d6ae3b558dc1f9088c1112b0ae89180c
stanuwijaya-creator <s.tanuwijaya@cqumail.com>
2026-06-05 17:18:08 +1000

    Update Sample_App.py

--- Prototype_MQTT/Sample_App.py
+++ Prototype_MQTT/Sample_App.py
@@ -97,26 +97,11 @@ def subscribe(client: mqtt_client):
     
 
 
-app = Dash()
 
 
-app.layout = [
-    html.Div(children='MQTT Broker Messages recording '),
-    dcc.Markdown('''  Made by :
-                 - Dev Anand Suresh (12272597)
-                 - Sai Teja Akula (12228905)
-                 - Suhartanto Tanuwijaya (12290667)
-                 '''),
- 
-    ]
-
-
-if __name__ == '__main__':
-    client = connect_mqtt()
-    subscribe(client)
-    client.loop_forever()
-    app.run(debug=True)
- 
+client = connect_mqtt()
+subscribe(client)
+client.loop_forever()
 
 
 
